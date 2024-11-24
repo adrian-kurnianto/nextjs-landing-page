99259ff367a80f6e9c7b81b9b1a8c865185e04d6
adrian-kurnianto <kurnianto.adrian23@gmail.com>
2024-11-24 21:17:03 +0700

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default async function Home() {
   const imageData = await fetch("https://www.giovankov.com/api/image.json");
   const productData = await fetch("https://www.giovankov.com/api/product.json");
 
-  let images = await imageData.json();
-  let product = await productData.json();
+  const images = await imageData.json();
+  const product = await productData.json();
 
   const modifiedProduct = product.data.map((el: ProductType, index: number) => {
     let imgStr;
-    let prdId = index + 1;
+    const prdId = index + 1;
 
     images.data.forEach((el: ImageType) => {
       const checked = el.id.some((idx) => idx === prdId.toString());
